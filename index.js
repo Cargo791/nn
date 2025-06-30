859f3c2fbde201554984df6763063d1d525a1a2b
Cargo791 <cargologo001@gmail.com>
2025-06-30 16:17:26 -0700

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pg from "pg";
 import fetch from "node-fetch";
 import path from "path"
 import { fileURLToPath } from "url";
